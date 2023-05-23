98c7a3056db4ea6e90493266d18da181481af0bb
ClaudeZsb <baobaobao369@163.com>
2023-05-23 15:32:35 +0800

    Update initial config

--- packages/contracts/src/lib/GameConfigInitializer.sol
+++ packages/contracts/src/lib/GameConfigInitializer.sol
@@ -3,24 +3,24 @@ pragma solidity >=0.8.0;
 
 import { IStore } from "@latticexyz/store/src/IStore.sol";
 import { GameConfigComponent } from "../codegen/Tables.sol";
+import { RemoveRuleType } from "../codegen/Types.sol";
 
 library GameConfigInitializer {
   function init(IStore world) internal { 
-    uint256 arg0 = 7;//slot
+    uint256 arg0 = 9;//slot
     arg0 += 2<<32;//composeNumbMin;
     arg0 += 7<<64;//composeNumbMax;
     arg0 += 16<<96;//typeNum;
-    arg0 += 16<<128;//levelBlockInitNum;
-    arg0 += 16<<160;//borderStep;
-    arg0 += 16<<192;//levelNum;
-    arg0 += 16<<224;//cardSize;
+    arg0 += 32<<128;//levelBlockInitNum;
+    arg0 += 4<<160;//borderStep;
+    arg0 += 8<<192;//levelNum;
+    arg0 += 44<<224;//cardSize;
  
     uint256 arg1 = 9;//viewWidth
     arg1 += 9<<32;//viewHeight
-    arg1 += 25<<64;//AlphabetNum
-    arg1 += 7<<96;//ColorNum
-
-    // uint256 configHash = uint256(keccak256(abi.encodePacked(arg0, arg1)));
+    arg1 += 25<<64;//TotalRangeNum
+    arg1 += 3<<96;//StageNum
+    arg1 += uint8(RemoveRuleType.Discrete)<<128;//RemoveRule
 
     GameConfigComponent.set(world, arg0, arg1);
   }
